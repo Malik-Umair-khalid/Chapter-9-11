cb19bbe7268ef74adce74d8e155160b64c966a5a
Malik-Umair-khalid <81845666+Malik-Umair-khalid@users.noreply.github.com>
2021-04-18 04:33:59 -0700

    Question 2 & 3 Completed

---
+++ app.js
@@ -0,0 +1,35 @@
+//1. Write a program to take “city” name as input from user. Ifuser enters “Karachi”, welcome the user like this:“Welcome to city of lights”
+
+
+// var city = prompt ("Enter Your City")
+
+// if (city === "Karachi") {
+//   alert ("Welcome To The City Of Lights")
+// }
+
+// 2.Write a program to take “gender” as input from user. If the user is male, give the message: Good Morning Sir. If the user is female, give the message: Good Morning Ma’am.
+
+// var gender = prompt ("Enter Your Gender")
+
+// if (gender === "male") {
+//     alert ("Good Morning Sir")
+// }
+// else if (gender === "female") {
+//     alert ("Good Morning Madam")
+// }
+
+// 3.Write a program to take input color of road traffic signal from the user & show the message according to this table
+
+// var signal = prompt ("Enter Signal Colour")
+// if (signal === "Red") {
+//     alert ("Must Stop")
+// }
+// if (signal === "Yellow" ) {
+//     alert ("Ready To Move")
+// }
+// if (signal === "green" ) {
+//     alert ("Move Now")
+// }
+// else {
+//     alert ("Try Agian")
+// }
